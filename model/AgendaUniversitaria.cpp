5da9608ebe7bd78e39b3f15b158c80775a51dfd0
lithium81689 <nicola.lolli@studenti.unipd.it>
2026-07-22 23:05:28 +0200

    minor fixes

--- model/AgendaUniversitaria.cpp
+++ model/AgendaUniversitaria.cpp
@@ -1,5 +1,18 @@
 #include "AgendaUniversitaria.h"
 
+AgendaUniversitaria::AgendaUniversitaria(
+    QString& titolo,
+    QString& descrizione,
+    QDate& data,
+    Priority priority,
+    bool completata
+    )
+    : titolo(titolo),
+    descrizione(descrizione),
+    data(data),
+    priority(priority),
+    completata(completata) {}
+
 
 QString AgendaUniversitaria::getTitolo() const {
     return titolo;
